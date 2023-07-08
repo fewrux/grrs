8284c835cd4311da79df3a81d967496ea823f935
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2023-07-08 00:25:47 -0300

    fix: adjust indicatif progress bar config

--- src/main.rs
+++ src/main.rs
@@ -31,20 +31,12 @@ fn main() -> Result<()> {
 
     let args = Cli::parse();
 
-    let mut builder = Builder::from_default_env();
-    match args.verbosity.log_level() {
-        Some(log::Level::Error) => builder.filter(None, log::LevelFilter::Error),
-        Some(log::Level::Warn) => builder.filter(None, log::LevelFilter::Warn),
-        Some(log::Level::Info) => builder.filter(None, log::LevelFilter::Info),
-        Some(log::Level::Debug) => builder.filter(None, log::LevelFilter::Debug),
-        Some(log::Level::Trace) => builder.filter(None, log::LevelFilter::Trace),
-        _ => builder.filter(None, log::LevelFilter::Off),
-    };
-    builder.init();
+    let mut builder = config_logger(&args);
 
-    let mut processed = 0;
+    let mut amount_processed = 0;
     let file_size = std::fs::metadata(&args.path)?.len();
-    let pb = get_progress_bar(file_size);
+
+    let pb = config_progress_bar(file_size);
 
     trace!("opening the file - size: {}", file_size);
     let file = File::open(&args.path)
@@ -56,8 +48,8 @@ fn main() -> Result<()> {
 
     trace!("reading the file");
     for line_result in reader.lines() {
-        let new = min(processed + (file_size / 100), file_size);
-        pb.set_position(new);
+        let new = min(amount_processed + (file_size / 100), file_size);
+        pb.inc(new);
 
         match line_result {
             Ok(line) => {
@@ -72,6 +64,7 @@ fn main() -> Result<()> {
                 eprintln!("could not read line: {}", e);
             }
         }
+        thread::sleep(Duration::from_millis(5));
     }
 
     pb.finish_with_message("Done!\n");
@@ -80,11 +73,25 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn get_progress_bar(file_size: u64) -> ProgressBar {
+fn config_progress_bar(file_size: u64) -> ProgressBar {
     let pb = ProgressBar::new(file_size);
     pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
         .unwrap()
         .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
         .progress_chars("#>-"));
     pb
+}
+
+fn config_logger(args: &Cli) -> Builder {
+    let mut builder = Builder::from_default_env();
+    match args.verbosity.log_level() {
+        Some(log::Level::Error) => builder.filter(None, log::LevelFilter::Error),
+        Some(log::Level::Warn) => builder.filter(None, log::LevelFilter::Warn),
+        Some(log::Level::Info) => builder.filter(None, log::LevelFilter::Info),
+        Some(log::Level::Debug) => builder.filter(None, log::LevelFilter::Debug),
+        Some(log::Level::Trace) => builder.filter(None, log::LevelFilter::Trace),
+        _ => builder.filter(None, log::LevelFilter::Off),
+    };
+    builder.init();
+    builder
 }
